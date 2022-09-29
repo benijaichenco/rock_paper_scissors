fcffb75291bbf57a6112ecc9aa2632db6459f878
faderb <benjaminjaichenco@gmail.com>
2022-09-29 19:24:00 +0300

    create script pseudocode

---
+++ script.js
@@ -0,0 +1,12 @@
+//create rock paper scissors variables,
+//create win lose draw variables,
+//create score variables,
+//generate function to return rock paper or scissors randomly,
+//generate function to return user's input,
+//generate function to start a rock paper scissors round against the computer
+//show computer selection on console log,
+//show player selection on console log,
+//generate if else for when player and computer choose rock paper or scissors,
+//show computer score on console log,
+//show player score on console log,
+//generate a function that invokes the game start function 5 times,
