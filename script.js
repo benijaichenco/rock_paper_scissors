3cd787f361e17682786c28f2fe6040dd8772b085
faderb <benjaminjaichenco@gmail.com>
2022-09-30 13:49:50 +0300

    generate playRound function and change score variables to functions

--- script.js
+++ script.js
@@ -2,13 +2,31 @@
 let rock;
 let paper;
 let scissors;
-//create win lose draw variables,
-let win;
-let lose;
-let tie;
 //create score variables,
-let compScore;
-let playerScore;
+let compScore = 0;
+let playerScore = 0;
+//create win lose draw variables,
+function win() {
+    console.log('Player won this round!');
+    playerScore += 1;
+    compScore += 0;
+    console.log('Player Score: ' + playerScore);
+    console.log('Computer Score: ' + compScore);
+}
+function lose() {
+    console.log('Computer won this round!');
+    playerScore += 0;
+    compScore += 1;
+    console.log('Player Score: ' + playerScore);
+    console.log('Computer Score: ' + compScore);
+}
+function tie() {
+    console.log('Draw!');
+    playerScore += 0;
+    compScore += 0;
+    console.log('Player Score: ' + playerScore);
+    console.log('Computer Score: ' + compScore);
+}
 //create choices variable,
 const choices = ['rock', 'paper', 'scissors'];
 //generate function to return rock paper or scissors randomly,
@@ -20,9 +38,34 @@ console.log(getCompChoice());
 function getPlayerChoice() {
     let input = prompt('type rock, paper or scissors:', '');
     input = input.toLowerCase();
-    console.log(input);
+    return input;
 }
 //generate function to start a rock paper scissors round against the computer,
+function playRound(playerSelection, compSelection) {
+    compSelection = getCompChoice();
+    console.log('Computer Selected: ' + compSelection);
+    playerSelection = getPlayerChoice();
+    console.log('Player selected: ' + playerSelection);
+    if (playerSelection == 'rock' && compSelection == 'rock') {
+        return tie();
+    } else if (playerSelection == 'rock' && compSelection == 'paper') {
+        return lose();
+    } else if (playerSelection == 'rock' && compSelection == 'scissors') {
+        return win();
+    } else if (playerSelection == 'paper' && compSelection == 'rock') {
+        return win();
+    } else if (playerSelection == 'paper' && compSelection == 'paper') {
+        return tie();
+    } else if (playerSelection == 'paper' && compSelection == 'scissors') {
+        return lose();
+    } else if (playerSelection == 'scissors' && compSelection == 'rock') {
+        return lose();
+    } else if (playerSelection == 'scissors' && compSelection == 'paper') {
+        return win();
+    } else if (playerSelection == 'scissors' && compSelection == 'scissors') {
+        return tie();
+    }
+}
 //show computer selection on console log,
 //show player selection on console log,
 //generate if else for when player and computer choose rock paper or scissors,
